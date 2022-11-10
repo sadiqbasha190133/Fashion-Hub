9c7333c9cdfaa22f0ea411bf30963be000847598
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-11-10 09:23:06 +0530

    fashion store

--- src/components/ReactSlick/index.js
+++ src/components/ReactSlick/index.js
@@ -3,7 +3,7 @@ import './index.css'
 
 const ReactSlick = () => {
   return (
-    <iframe width="1450" height="750" 
+    <iframe width="1450" height="750" title='video-slick'
         src="https://www.youtube.com/embed/cN9306z4_hU?autoplay=1&mute=1&controls=0">
     </iframe>
   )
