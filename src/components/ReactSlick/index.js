33d9189d7db5e1550a66dd6130f25f43321bb39e
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-11-12 19:00:56 +0530

    fashion-store

--- src/components/ReactSlick/index.js
+++ src/components/ReactSlick/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './index.css'
+import ReactPlayer from 'react-player/youtube'
 
 const ReactSlick = () => {
   return (
-    <iframe width="1450" height="750" title='video-slick'
-        src="https://www.youtube.com/embed/cN9306z4_hU?autoplay=1&mute=1&controls=0">
-    </iframe>
+    <div className='react-slick-bg'>
+      <ReactPlayer url="https://www.youtube.com/embed/cN9306z4_hU?autoplay=1&mute=1&controls=0" 
+      width='99vw' 
+      height='90vh'
+      playing = 'true'
+      muted = 'false'
+      loop = 'true'
+      />
+    </div>
   )
 }
 
